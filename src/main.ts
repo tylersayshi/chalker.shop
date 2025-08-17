cc7ee5a69917bbf87551e735ba0febc1c7046a93
Tyler <26290074+tylersayshi@users.noreply.github.com>
2025-08-16 23:10:39 -0700

    ready with doodle

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+const addEmailListener = () => {
+  const emailInput = document.querySelector("#bd-email") as HTMLInputElement;
+  const audio = new Audio("chalkboard.mp3");
+  audio.preload = "auto";
+  const goodChalkStarts = [2, 3.5, 5.8, 7, 8.4, 9.2];
+
+  if (!emailInput) return;
+  emailInput.addEventListener("keydown", (event) => {
+    if (event.key === "Space" || event.key === "Backspace") {
+      return;
+    }
+    let isChalking = false;
+    const playChalk = () => {
+      // one at a time
+      if (isChalking) return;
+      const start =
+        goodChalkStarts[Math.floor(Math.random() * goodChalkStarts.length)];
+      console.log({ start });
+      audio.currentTime = start;
+      audio.play();
+      setTimeout(() => {
+        audio.pause();
+        isChalking = false;
+      }, 500);
+    };
+    // Play a random 0.5s segment from chalkboard.mp3
+    audio.addEventListener("loadedmetadata", playChalk);
+    // If metadata is already loaded
+    if (audio.readyState >= 1) {
+      playChalk();
+    }
+  });
+};
+
+addEmailListener();
